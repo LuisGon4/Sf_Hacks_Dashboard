6c8dab507ab33f541fdfcffa02402e626c00be8d
josue <jcruz323@mail.ccsf.edu>
2026-02-27 17:56:46 -0800

    feat: add routing for underlying app root components

--- Hackathon_Frontend/src/App.jsx
+++ Hackathon_Frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import { Dashboard } from './components/layout/Dashboard';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // purpose: main page to link from admin or greensense dashboard
+import { Dashboard } from './components/layout/Dashboard.jsx';
+import { AdminPage } from './components/layout/RouterAdmin.jsx';
+// direction admin panel for React app, making references to admin alongside added in sub webpages from admin page
 
 function App() {
-  return <Dashboard />;
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AdminPage />} /> {/* Admin start page part of app*/}
+          <Route path="/greensense-dashboard" element={<Dashboard />} />  {/* GreenSense App part of app */}
+
+        </Routes>
+      </BrowserRouter>
+  );
 }
 
 export default App;
