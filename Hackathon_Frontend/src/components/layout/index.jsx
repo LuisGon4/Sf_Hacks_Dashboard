bead47356bae8f3b3f2c9aaabb873f4b2866cc69
josue <jcruz323@mail.ccsf.edu>
2026-03-01 23:18:22 -0800

    feat: initial barrel import(table of contents) for outgoing imports on other files of each component

---
+++ Hackathon_Frontend/src/components/layout/index.jsx
@@ -0,0 +1,3 @@
+// purpose: best practice in being the index.jsx of imports toward App.jsx instead of absolute path imports (table of contents for outgoing exports)
+export { Dashboard } from './Dashboard.jsx';
+export { AdminPage } from './RouterAdmin.jsx'
